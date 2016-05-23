1a66427ba6e9f66da74fe45f8aae44ffc54b1cd2
matianfu <matianfu@gmail.com>
2016-05-23 14:51:12 +0800

    add apps.json and gen script

---
+++ genAppsJson.js
@@ -0,0 +1,19 @@
+import fs from 'fs'
+import apps from './lib/apps'
+
+try {
+
+  let json = JSON.stringify(apps, null, '  ')
+  fs.writeFile("./hosted/apps.json", json, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+
+    console.log("The file was saved!");
+  }); 
+}
+catch (e) {
+  console.log(e)
+}
+
+
