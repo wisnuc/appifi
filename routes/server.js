a23d0bb53b0edd0db30ca4f16a888782dc5ef67f
matianfu <matianfu@gmail.com>
2016-06-03 22:55:13 +0800

    tmp

--- routes/server.js
+++ routes/server.js
@@ -5,5 +5,10 @@ const router = express.Router()
 
 router.get('/', (req, res) => res.status(200).json(server.get()))
 router.get('/status', (req, res) => res.status(200).json(server.status()))
+router.post('/', (req, res) =>  
+  server.operation(req.body, (err, result) => 
+    err ? res.status(500).json(err) :
+      res.status(200).json(result)))
 
 module.exports = router
+
