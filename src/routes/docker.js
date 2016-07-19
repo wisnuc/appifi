9ed52ba5815a40fbd9c14570cf8f4bca26cd2f0e
lewis ma <lewis.ma@winsuntech.cn>
2016-07-19 16:12:47 +0800

    remove server side get method from docker router

--- src/routes/docker.js
+++ src/routes/docker.js
@@ -3,10 +3,6 @@ import docker from '../lib/docker'
 
 const router = express.Router()
 
-router.get('/', (req, res) => res.status(200).json(docker.get()))
-
-router.get('/status', (req, res) => res.status(200).json(docker.status()))
-
 router.post('/', (req, res) =>  
   docker.operation(req.body, (err, result) => 
     err ? res.status(500).json(err) :
