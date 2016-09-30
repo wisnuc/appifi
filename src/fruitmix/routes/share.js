3ca3f44b30ebf67e9d5f0b9626bfdbc3eca8fb3e
lewis ma <lewis.ma@winsuntech.cn>
2016-09-30 17:05:04 +0800

    minor

--- src/fruitmix/routes/share.js
+++ src/fruitmix/routes/share.js
@@ -8,9 +8,7 @@ const router = Router()
 router.get('/sharedWithMe', auth.jwt(), (req, res) => {
 
   let forest = Models.getModel('forest')    
-
   let user = req.user
-
   let shared = forest.getSharedWithMe(user.uuid) 
   res.status(200).json(shared)
 })
@@ -19,7 +17,6 @@ router.get('/sharedWithOthers', auth.jwt(), (req, res) => {
 
   let forest = Models.getModel('forest')
   let user = req.user
-
   let shared = forest.getSharedWithOthers(user.uuid)
   res.status(200).json(shared)
 })
