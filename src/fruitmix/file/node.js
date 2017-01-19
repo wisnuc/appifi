19d8def5ef3c696cb82f7b09140f7196516f40c1
lewis ma <lewis.ma@winsuntech.cn>
2017-01-19 11:33:38 +0800

    try standard

--- src/fruitmix/file/node.js
+++ src/fruitmix/file/node.js
@@ -3,76 +3,75 @@ import validator from 'validator'
 // tree, link-list => recursive
 // array, => linear
 // for (let x= node; x.parent !== null; x = x.parent)
-// 
+//
 // turing machine vs lambda calculus / functional programming
 // lambda expression () => {}
 //
 
 class Node {
 
-  constructor(props) {
+  constructor (props) {
     // TODO
     Object.assign(this, props)
     this.parent = null
     this.children = [] // new Set
   }
 
-  attach(parent) {
+  attach (parent) {
     if (this.parent) throw new Error('node is already attached')
     this.parent = parent
     parent.setChild(this)
   }
 
-  detach() {
+  detach () {
     if (this.parent === null) throw new Error('Node is already detached')
     this.parent.unsetChild(this)
-    this.parent = null   
+    this.parent = null
   }
 
-  setChild(child) {
+  setChild (child) {
     this.children ? this.children.push(child) : this.children = [child]
   }
 
-  unsetChild(child) {
-
+  unsetChild (child) {
     let children = this.children
-    if (children === undefined) 
-      throw new Error('Node has no children')
+    if (children === undefined) { throw new Error('Node has no children') }
 
     let index = children.findIndex(c => c === child)
     if (index === -1) throw new Error('Node has no such child')
     children.splice(index, 1)
 
-    if (children.length === 0) 
-      delete this.children 
+    if (children.length === 0) {
+      delete this.children
+    }
   }
 
-  root() {
+  root () {
     let node = this
     while (node.parent !== null) node = node.parent
     return node
   }
 
-  nea() {
-    let node = this  
+  nea () {
+    let node = this
     while (!node.writelist) node = node.parent
     return node
   }
 
-  isRootOwner(userUUID) {
+  isRootOwner (userUUID) {
     return (
       this.root().owner.indexOf(userUUID) !== -1
     )
-  } 
+  }
 
-  userWritable(userUUID) {
+  userWritable (userUUID) {
     return (
       this.root().owner.indexOf(userUUID) !== -1 ||
       this.nea().writelist.indexOf(userUUID) !== -1
     )
   }
 
-  userReadable(userUUID) {
+  userReadable (userUUID) {
     return (
       this.root().owner.indexOf(userUUID) !== -1 ||
       this.nea().writelist.indexOf(userUUID) !== -1 ||
@@ -81,11 +80,11 @@ class Node {
   }
 
   // always return array
-  getChildren() {
-    return this.children ? this.children : [] 
+  getChildren () {
+    return this.children ? this.children : []
   }
 
-  upEach(func) {
+  upEach (func) {
     let node = this
     while (node !== null) {
       func(node)
@@ -93,7 +92,7 @@ class Node {
     }
   }
 
-  upFind(func) {
+  upFind (func) {
     let node = this
     while (node !== null) {
       if (func(node)) return node
@@ -101,78 +100,80 @@ class Node {
     }
   }
 
-  nodepath() {
+  nodepath () {
     let q = []
     this.upEach(node => q.unshift(node))
     return q
-  } 
+  }
 
-  namepath() {
+  namepath () {
     return path.join(...this.nodepath().map(n => n.name))
   }
 
-  walkDown(names) {
+  walkDown (names) {
     if (names.length === 0) return this
     let named = this.getChildren().find(child => child.name === names[0])
     if (!named) return this
     return named.walkDown(names.slice(1))
   }
 
-  preVisit(func) {
+  preVisit (func) {
     func(this)
-    if (this.children) 
-      this.children.forEach(child => child.preVisit(func)) 
+    if (this.children) {
+      this.children.forEach(child => child.preVisit(func))
+    }
   }
 
-  postVisit(func) {
-    if (this.children)
+  postVisit (func) {
+    if (this.children) {
       this.children.forEach(child => child.postVisit(func))
-    func(this) 
+    }
+    func(this)
   }
 
-  preVisitEol(func) {
-    if (func(this) && this.children)
-      this.children.forEach(child => child.preVisitEol(func))  
+  preVisitEol (func) {
+    if (func(this) && this.children) {
+      this.children.forEach(child => child.preVisitEol(func))
+    }
   }
 
-  preVisitFind(func) {
+  preVisitFind (func) {
     if (func(this)) return this
     if (this.getChildren().length === 0) return undefined
     return this.children.find(child => child.preVisitFind(func))
   }
 
-  isFile() {
+  isFile () {
     return this.type === 'file'
   }
 
-  isDirectory() {
+  isDirectory () {
     return this.type === 'folder'
   }
 }
 
 class FileNode extends Node {
 
-  constructor(props) {
+  constructor (props) {
     super(props)
   }
-} 
+}
 
 class FolderNode extends Node {
 
-  constructor(props) {
-    super(props)    
+  constructor (props) {
+    super(props)
   }
 }
 
 // node.type ==== 'file'
 // node instanceof FileNode
 
-const isUUID = uuid => 
+const isUUID = uuid =>
   typeof uuid === 'string' && validator.isUUID(uuid)
 
 // throw error
 const createNode = props => {
-
   // props must have uuid
   if (!isUUID(props.uuid)) {
     let e = new Error('invalid uuid')
@@ -188,7 +189,7 @@ const createNode = props => {
   }
 
   // TODO validate owner, writelist, readlist, name
-  // if file, size & mtime 
+  // if file, size & mtime
   // if folder ???? TODO
   return new Node(props)
 }
@@ -204,7 +205,7 @@ root
 
 timestamp = 0
 
-probe node 
+probe node
 
 1. read timestamp ts1
 2. probe folders and files inside given folder
@@ -220,11 +221,9 @@ folder a
 
 folder a / file b (rename b1) probe -> a
 
-
 folder node -> concurrent probe request
 
 schedule
-
 
 waiting <- probe (wait another 50ms)
         <- timeout (go to probing 0)
@@ -268,43 +267,4 @@ latency reconciliation time
 statistic probe time? io time?
 
 ***/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
