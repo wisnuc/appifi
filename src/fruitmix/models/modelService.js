3b10bcb7e2718dfc28ca4680bd8d77aebd8a126a
panda <wangpanhn@qq.com>
2017-03-15 17:50:28 +0800

    modelService

--- src/fruitmix/models/modelService.js
+++ src/fruitmix/models/modelService.js
@@ -1,6 +1,43 @@
 
-Promise.promisifyAll(fs);
-const mkdirpAsync = Promise.promisify(mkdirp);
+import E from '../lib/error'
+import path from 'path'
+import fs from 'fs'
+import UUID from 'node-uuid'
+import bcrypt from 'bcrypt'
+import { md4Encrypt } from '../tools'
+import ursa from 'ursa'
+
+const passwordEncrypt = (password, saltLen) =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(saltLen));
+
+const getCredentials = () => {
+  let key         = ursa.generatePrivateKey(512, 65537);
+  let privatePem  = ursa.createPrivateKey(key.toPrivatePem());
+  let privateKey  = privatePem.toPrivatePem('utf8');
+  let publicPem   = ursa.createPublicKey(key.toPublicPem());
+  let publicKey   = publicPem.toPublicPem('utf8');
+  return { publicKey, privateKey }
+}
+
+const mergeAndDedup = (arr1, arr2) => {
+  let arr = [];
+  let set = new Set([...arr1, ...arr2]);
+  for (let i of set)
+    arr.push(i);
+  return arr;
+}
+
+const arrDeleteArr = (arr1, arr2) => {
+  let set = new Set(arr1);
+  arr2.forEach(i => {
+    if (set.has(i))
+      set.delete(i);
+  });
+  let arr = [];
+  for (let i of set)
+    arr.push(i);
+  return arr;
+}
 
 const isExist = (target, callback) =>
   fs.access(target, fs.constants.R_OK, err =>
@@ -19,83 +56,269 @@ const fileToJsonAsync = Promise.promisify(fileToJson);
 
 class ModelService {
 
-	createLocalUser(user, props, callback) {
-		// check permission
-		// check args
-		// dispatch
-	}
-
-	createRemoteUser(user, props, callback) {}
-
-	updateLocalUser(user, props, callback) {}
-
-	updateRemoteUser(user, props, callback) {}
-
-	updatePassword(user, password, callback) {}
-
-	// friends is an array
-	createFriend(user, friends, callback) {}
-
-	// friends is an array
-	deleteFriend(user, friends, callback) {}
-
-	createPublicDrive(user, props, callback) {}
-
-	updatePublicDrive(user, props, callback) {}
-
-	deletePublicDrive(user, driveuuid, callback) {}
-
+  constructor(modelData) {
+    this.modelData = modelData;
+  }
 
   // TODO functional
-	allocUnixUID() {
-		while (this.eset.has(this.increment)) this.increment++
-    return this.increment++
-	}
+  allocUnixUID() {
+    let uids = this.modelData.users.filter(u => u.type === 'local').map(u => u.unixuid);
+    let set = new Set(uids);
+    let uid = 2000;
+    while(set.has(uid)) { uid++; }
+    return uid;
+  }
 
   // opaque
-	async initializeAsync() {
+  async initializeAsync() {
 
-		if (this.lock) throw new E.ELOCK();
-    this.getLock();
+    if (this.modelData.lock) throw new E.ELOCK();
+    this.modelData.getLock();
     let err = null;
-    let modelInfo;
-    let dirty = false;
-    let existM = await isExistAsync(this.modelFilePath);
+    let users, drives;
+    let modelPath = this.modelData.modelPath;
+    let existM = await isExistAsync(modelPath);
+
     if (existM){
       // model.json exist
       try {
-        modelInfo = await fileToJsonAsync(this.modelFilePath);
-        if (!Array.isArray(modelInfo.users) || !Array.isArray(modelInfo.drives) || modelInfo.version === undefined)
-          err = new E.EDOCFORMAT('Model file format is not correct');
+        let modelInfo = await fileToJsonAsync(modelPath);
+        users = modelInfo.users;
+        drives = modelinfo.drives;
       } catch (e) { err = e; }
     } else {
-      // model.json non-existent, read user.json & drive.json
-      dirty = true;
+      // read user.json & drive.json
       try {
-        let users = await fileToJsonAsync(this.userFilePath);
-        let drives = await fileToJsonAsync(this.drivefilePath);
-        if(!Array.isArray(users) || !Array.isArray(drives))
-          // format is not correct
-          err = new E.EDOCFORMAT('user or drive file format is not correct');
-        else 
-          modelInfo = Object.assign({}, { version: 1, users, drives });
+        users = await fileToJsonAsync(path.join(path.dirname(modelPath), 'user.json'));
+        drives = await fileToJsonAsync(path.join(path.dirname(modelPath), 'drive.json'));
       } catch (e) {
         if (e.code !== 'ENOENT')
           err = e;
-        else
-          modelInfo = Object.assign({}, { version: 1, users: [], drives: [] });
+        else {
+          users = [];
+          drives = [];
+        }
       }
     }
-    this.putLock();
     if (err) throw err;
-    // save or not save
-    if (dirty) await this.saveAsync(modelInfo);
-    // set model users & drives data
-    this.setModelData(modelInfo);
-    this.users.forEach(user => {
-      if (user.type === 'local')
-        this.eset.add(user.unixUID);
-    });
+    await this.modelData.updateModelAsync(users, drives);
+  }
+
+	async createLocalUserAsync(useruuid, props) {
+    // check permission
+    let users = this.modelData.users;
+    let admins = user.filter(u => u.isAdmin === true)
+    if (users.length !== 0 && !admins.includes(useruuid))
+      throw new Error('must be an administrator to create a user');
+    if (props.type !== 'local')
+      throw new Error('the new user type must be local');
+    // install newUser
+    let {
+      type, username, password, nologin, isFirstUser,
+      isAdmin, email, avatar, unixname
+    } = props;
+    // user uuid
+    let uuid = UUID.v4();
+
+    if (nologin !== true) nologin = false;
+    if (isAdmin !== true) isAdmin = false;
+    if (user.length === 0){
+      isFirstUser = true;
+      isAdmin = true;
+    }
+
+    email = email || null;
+    avatar = avata || null;
+
+    let home = UUID.v4();
+    let library = UUID.v4();
+    let service = UUID.v4();
+    // alloc unix uid
+    let unixuid = this.allocUnixUID();
+    let lastChangeTime = new Date().getTime();
+
+    let passwordEncrypted = passwordEncrypt(password, 10);
+    let unixPassword = passwordEncrypt(password, 8);    //???
+    let smbPassword = md4Encrypt(password);
+
+    let friends = [];
+    // get credentials
+    let credentials = getCredentials();
+
+    let newUser = {
+      type, uuid, username, nologin, isFirstUser, isAdmin,
+      email, avatar, home, library, service, unixuid,
+      unixname, lastChangeTime, credentials, friends,
+      unixPassword, smbPassword,
+      password: passwordEncrypted, 
+    };
+    // install newDrives
+    let common = { owner: uuid, type: 'private' };
+    let homeDrive     = Object.assign({}, common, { uuid: home,     label: 'home' });
+    let libraryDrive  = Object.assign({}, common, { uuid: library,  label: 'library' });
+    let serviceDrive  = Object.assign({}, common, { uuid: service,  label: 'service' });
+    let newDrives = [ homeDrive, libraryDrive, serviceDrive ];
+    try {
+      await this.modelData.createUserAsync(newUser, newDrives);
+    } catch (e) { throw e; }
 	}
 
+	async createRemoteUserAsync(useruuid, props) {
+    // check permission
+    let users = this.modelData.users;
+    let admins = user.filter(u => u.isAdmin === true)
+    if (!admins.includes(useruuid)) throw new Error('must be an administrator to create a user');
+    if (props.type !== 'remote') throw new Error('the new user type must be remote');
+    // install newUser
+    let type = 'remote';
+    let uuid = UUID.v4();
+    let email = props.email || null;
+    let avatar = props.email || null;
+    let service = UUID.v4();
+    let lastChangeTime = new Date.getTime();
+    let newUser = { type, uuid, username, email, avatar, service, lastChangeTime };
+    // install newdrives
+    let newDrives = {
+      label: 'service',
+      uuid: service,
+      type: 'service'
+    }
+    try {
+      await this.modelData.createUserAsync(newUser, newDrives);
+    } catch (e) { throw e; }
+  }
+
+	async updateLocalUser(useruuid, props) {
+    // check permission
+    let users = this.modelData.users;
+    let admins = user.filter(u => u.isAdmin === true);
+    // Not an administrator && not oneself
+    if (!admins.includes(useruuid) && porps.uuid !== useruuid)
+      throw new Error('no permission to modify user information');
+    // install user
+    let user = users.find(u => u.uuid === props.uuid);
+    let lastChangeTime = new Date().getTime();
+    let next = Object.assign({}, user, props, { lastChangeTime });
+    try {
+      await this.modelData.updateUserAsync(next);
+    } catch (e) { throw e; }
+  }
+
+  // props: { password, uuid }
+	async updatePasswordAsync(useruuid, props) {
+    // check permission
+    let users = this.modelData.users;
+    let admins = user.filter(u => u.isAdmin === true);
+    // Not an administrator && not oneself
+    if (!admins.includes(useruuid) && props.uuid !== useruuid)
+      throw new Error('no permission to modify user password');
+    // install user
+    let user = users.find(u => u.uuid === props.uuid);
+
+    let password = passwordEncrypt(props.password, 10);
+    let unixPassword = passwordEncrypt(props.password, 8);    //???
+    let smbPassword = md4Encrypt(props.password);
+    let lastChangeTime = new Date.getTime();
+
+    let next = Object.assign({}, user, { password, unixPassword, smbPassword, lastChangeTime });
+    try {
+      await this.modelData.updateUserAsync(next, pwd);
+    } catch (e) { throw e; }
+  }
+
+	// props: { uuid, friends: [] }
+	async createFriendAsync(useruuid, props) {
+    // check permission
+    let users = this.modelData.users;
+    let admins = user.filter(u => u.isAdmin === true);
+    // Not an administrator && not oneself
+    if (!admins.includes(useruuid) && props.uuid !== useruuid)
+      throw new Error('no permission to create friend');
+    // install user
+    let user = users.find(u => u.uuid === props.uuid);
+    let newFriends = mergeAndDedup(user.friends, props.friends);
+    let lastChangeTime = new Date().getTime();
+
+    let next = Object.assign({}, user, { friends: newFriends, lastChangeTime });
+    try {
+      await this.modelData.updateUserAsync(next);
+    } catch (e) { throw e; }
+  }
+
+	// props: { uuid, friends: [] }
+	async deleteFriendAsync(useruuid, props) {
+    // check permission
+    let users = this.modelData.users;
+    let admins = user.filter(u => u.isAdmin === true);
+    // Not an administrator && not oneself
+    if (!admins.includes(useruuid) && props.uuid !== useruuid)
+      throw new Error('no permission to delete friend');
+    // install user
+    let user = users.find(u => u.uuid === props.uuid);
+    let newFriends = arrDeleteArr(user.friends, props.friends);
+    let lastChangeTime = new Date().getTime();
+
+    let next = Object.assign({}, user, { friends: newFriends, lastChangeTime });
+    try {
+      await this.modelData.updateUserAsync(next);
+    } catch (e) { throw e; }
+  }
+
+	async createPublicDriveAsync(useruuid, props) {
+    // check permission ---- all user can create public drive ???
+    let users = this.modelDate.users;
+    if (!users.includes(useruuid))
+      throw new Error('no permission to create public drive');
+    //install new drive
+    let uuid = UUID.v4();
+    let type = props.type;
+    let writelist = props.writelist || [];
+    let readlist = props.readlist || [];
+    let shareAllowed = props.shareAllowed === true ? true : false;
+
+    let newDrive = { uuid, type, writelist, readlist, shareAllowed };
+    try {
+      await this.modelData.createDriveAsync(newDrive);
+    } catch (e) { throw e; }
+  }
+
+	async updatePublicDriveAsync(useruuid, props) {
+    // check permission  --- all user can update public drive ???
+    let users = this.modelDate.users;
+    if (!users.includes(useruuid))
+      throw new Error('no permission to create public drive');
+    // install next drive
+    let drives = this.modelData.drives;
+    let drive = drives.find(d => d.uuid = props.uuid);
+
+    let next = Object.assign({}, drive, props);
+    try {
+      await this.modelData.updateDriveAsync(next);
+    } catch (e) { throw e; }
+  }
+
+  // props: { driveuuid }
+	async deletePublicDriveAsync(useruuid, props) {
+    // check permission  --- all user can delete public drive ???
+    let users = this.modelDate.users;
+    if (!users.includes(useruuid))
+      throw new Error('no permission to create public drive');
+    // delete
+    try {
+      await this.modelData.deleteDriveAsync(props.driveuuid);
+    } catch (e) { throw e; }
+  }
+
 }
+
+const createModelService = async (modelData, callback) => {
+  let modelService = new ModelService(modelData);
+  try {
+    await modelService.initializeAsync();
+    callback(null, modelService);
+  } catch (e) { callback(e) }
+}
+const createModelServiceAsync = Promise.promisify(createModelService);
+
+export default createModelServiceAsync
+
