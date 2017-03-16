da4484d598d1582f734ba9d98616d35b6fae1b24
CJackYang <ijackyang@sina.cn>
2017-03-16 18:28:24 +0800

    modified:   src/fruitmix/fruitmix.js

--- src/fruitmix/main.js
+++ src/fruitmix/main.js
@@ -1,11 +1,16 @@
 import http from 'http'
 import cluster from 'cluster'
 import os from 'os'
+
 import ipcMain from './lib/ipcMain'
 import ipcWorker from './lib/ipcWorker'
+import app from './app'
+import { createFruitmix, createHttpServer } from './fruitmix'
 
 if (cluster.isMaster) {
   // init data source
+  // sysroot from child_process.fork option
+  createFruitmix(process.env.sysroot)
 
   //create workers
   os.cpus().forEach(() => cluster.fork())
@@ -16,6 +21,7 @@ if (cluster.isMaster) {
 }else if(cluster.isWorker){
   //start ipc
   ipcWorker.start()
-  //create sever
   
+  //create sever
+  createHttpServer(app, () => console.log('fruitmix worker:' + cluster.worker.id + ' PID: ' + process.pid + ' start on port :' + app.get('port')))
 }
