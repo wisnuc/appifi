befdff8aa7d5b28ba6c1acdc637cadaa1e86b77e
CJackYang <ijackyang@sina.cn>
2017-03-17 12:05:34 +0800

    tmp

---
+++ src/fruitmix/fruitmix_worker.js
@@ -0,0 +1,14 @@
+import http from 'http'
+import cluster from 'cluster'
+
+import ipcWorker from './lib/ipcWorker'
+import app from './app'
+import { createHttpServer } from './fruitmix'
+
+if(cluster.isWorker){
+  //start ipc
+  ipcWorker.start()
+  
+  //create sever
+  createHttpServer(app, () => console.log('fruitmix worker:' + cluster.worker.id + ' PID: ' + process.pid + ' start on port :' + app.get('port')))
+}
--- src/fruitmix/main.js
+++ src/fruitmix/main.js
@@ -3,25 +3,20 @@ import cluster from 'cluster'
 import os from 'os'
 
 import ipcMain from './lib/ipcMain'
-import ipcWorker from './lib/ipcWorker'
-import app from './app'
-import { createFruitmix, createHttpServer } from './fruitmix'
+import { createFruitmix } from './fruitmix'
 
 if (cluster.isMaster) {
   // init data source
   // sysroot from child_process.fork option
   createFruitmix(process.env.sysroot)
 
-  //create workers
-  os.cpus().forEach(() => cluster.fork())
-
   //start ipc
   ipcMain.start()
 
-}else if(cluster.isWorker){
-  //start ipc
-  ipcWorker.start()
+  cluster.setupMaster({exec:'fruitmix_worker.js'})
+  //create workers
+  os.cpus().forEach(() => cluster.fork())
+
+  // on worker events
   
-  //create sever
-  createHttpServer(app, () => console.log('fruitmix worker:' + cluster.worker.id + ' PID: ' + process.pid + ' start on port :' + app.get('port')))
 }
