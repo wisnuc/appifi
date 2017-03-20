386a6ed0e84b3ee055ddb55f3cc3e6a9b764bd14
matianfu <matianfu@gmail.com>
2017-03-20 09:28:29 +0800

    add nodeify, add login router

---
+++ src/fruitmix/cluster/routes/login.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+
+const { localUsers } = require('../model')
+
+router.get('/', (req, res) => {
+
+  localUsers((err, users) => {
+
+    if (err) {
+      return res.status(500).json({
+        err: {
+          code: err.code,
+          message: err.message
+        }
+      })
+    }
+
+    res.status(200).json(users.map(u => ({
+      uuid: u.uuid,
+      username: u.username,
+      avatar: user.avatar,
+      unixUID: user.unixUUID
+    })))
+  })
+})
+
+module.exports = router
+
