d08ecf0055c2ffb460b34dc1800c60c488d4a9fb
CJackYang <ijackyang@sina.cn>
2017-03-23 15:35:30 +0800

    tmp

--- src/fruitmix/cluster/lib/filemap.js
+++ src/fruitmix/cluster/lib/filemap.js
@@ -2,22 +2,37 @@ import path from 'path'
 import fs from 'fs'
 import child from 'child_process'
 
+import xattr from 'fs-xattr'
+
 import  paths from './paths'
 
-const creatSegmentsFileAsync = async ({ size, segmentsize, nodeuuid, sha256, name, useruuid} , callback) => {
+Promise.promisifyAll(child)
+Promise.promisifyAll(xattr)
+
+let FILEMAP = 'user.filemap'
+
+const creatSegmentsFileAsync = async ({ size, segmentsize, nodeuuid, sha256, name, useruuid}) => {
   // fallocate -l 10G bigfile
   let folderPath = path.join(paths.get('filemap'), useruuid)
   try{
     await fs.mkdirAsync(folderPath)
-  }catch(e){ callback(e) }
-  let filepath = path.join(folderPath, sha256)
-  child.exec('fallocate -l ' + size +' ' + filepath)
-
-  
+    let filepath = path.join(folderPath, sha256)
+    await child.execAsync('fallocate -l ' + size +' ' + filepath)
+    // may throw xattr ENOENT or JSON SyntaxError
+    // attr = JSON.parse(await xattr.getAsync(target, FRUITMIX))
+    let segments = []
+    for(let i = 0; i < Math.ceil(size/segmentsize); i++){
+      segments.push(0)
+    }
+    let attr = { size, segmentsize, segments, nodeuuid, sha256, name }
+    await xattr.setAsync(filepath, FILEMAP, JSON.stringify(attr))
+    return attr
+  }catch(e){ throw e }
 }
 
-const updateSegmentsFile = ({ sha256, stream, start }, callback) => {
-
+const updateSegmentsFile = ({ sha256, stream, start, useruuid }, callback) => {
+  let folderPath = path.join(paths.get('filemap'), useruuid, sha256)
+  fs.createReadStream()
 }
 
 export { creatSegmentsFile, updateSegmentsFile }
