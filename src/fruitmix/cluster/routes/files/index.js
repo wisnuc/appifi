7b3e0efc2c2db0f33700a4c856e4ef760d9b9056
matianfu <matianfu@gmail.com>
2017-03-29 00:46:29 +0800

    tmp

--- src/fruitmix/cluster/routes/files/index.js
+++ src/fruitmix/cluster/routes/files/index.js
@@ -6,8 +6,13 @@
 import { Router } from 'express'
 import fruitmix from './fruitmix'
 
+const external = require('./external')
+const transfer = require('./transfer')
+
 let router = Router()
 
 router.use('/fruitmix', fruitmix)
+router.use('/external', external)
+router.use('/transfer', transfer)
 
 export default router
