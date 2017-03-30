b670237bbf5e1c4c6de87659f451384d18344eeb
fangqiaoyun <854079504@qq.com>
2017-03-30 16:37:47 +0800

    modify fileShareData

--- src/fruitmix/file/fileShareData.js
+++ src/fruitmix/file/fileShareData.js
@@ -57,8 +57,12 @@ class FileShareData extends EventEmitter {
     this.fileData = fileData
   }
 
-  load() {
-
+  async load() {
+    let shares = await this.fss.retrieveAllAsync()
+    shares.forEach(share => {
+      this.fsMap.set(share.doc.uuid, share)
+    })
+    this.emit('fileShareCreated', shares)
   }
 
   userAuthorizedToRead(userUUID, node) { // starting from root
@@ -107,7 +111,7 @@ class FileShareData extends EventEmitter {
     let fileShare = new FileShare(digest, doc)
 
     this.fsMap.set(doc.uuid, fileShare)
-    this.emit('fileShareCreated', fileShare)
+    this.emit('fileShareCreated', [fileShare])
     return fileShare
   }
 
@@ -141,7 +145,9 @@ class FileShareData extends EventEmitter {
 
 const createFileShareData = (model, fileShareStore) => {
   Promise.promisifyAll(fileShareStore)
-  return new FileShareData(model, fileShareStore)
+  let fileShareData = new FileShareData(model, fileShareStore)
+  await fileShareData.load()
+  return fileShareData
 }
 
 export { createFileShareData }
