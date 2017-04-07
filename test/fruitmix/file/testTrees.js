fc83214909eefb41b5d088bcd731b0fd0b0bacef
jianjin.wu <mosaic101@foxmail.com>
2017-04-07 10:54:54 +0800

    temp

--- test/fruitmix/file/testTrees.js
+++ test/fruitmix/file/testTrees.js
@@ -23,7 +23,7 @@
 
 import EventEmitter from 'events'
 
-const uuids = {
+const uuidMap = {
   'userUUID': 'c9f1d82e-5d88-46d7-ad43-24d51b1b6628',
   'aliceUUID': 'b9aa7c34-8b86-4306-9042-396cf8fa1a9c',
   'bobUUID': 'f97f9e1f-848b-4ed4-bd47-1ddfa82b2777',
@@ -46,14 +46,14 @@ class Model extends EventEmitter {
   constructor() {
     super()
   }
-
   getUsers() {
-    return [ {uuid: 'c9f1d82e-5d88-46d7-ad43-24d51b1b6628', type: 'local'},
-             {uuid: 'b9aa7c34-8b86-4306-9042-396cf8fa1a9c', type: 'local'},
-             {uuid: 'f97f9e1f-848b-4ed4-bd47-1ddfa82b2777', type: 'local'},
-             {uuid: 'e5f23cb9-1852-475d-937d-162d2554e22c', type: 'local'},
-             {uuid: 'b8ff0e08-0acb-4013-8129-a4d913e79339', type: 'remote'},
-           ]
+    return [
+      { uuid: 'c9f1d82e-5d88-46d7-ad43-24d51b1b6628', type: 'local' },
+      { uuid: 'b9aa7c34-8b86-4306-9042-396cf8fa1a9c', type: 'local' },
+      { uuid: 'f97f9e1f-848b-4ed4-bd47-1ddfa82b2777', type: 'local' },
+      { uuid: 'e5f23cb9-1852-475d-937d-162d2554e22c', type: 'local' },
+      { uuid: 'b8ff0e08-0acb-4013-8129-a4d913e79339', type: 'remote' }
+    ]
   }
 }
 
@@ -61,50 +61,51 @@ let model = new Model()
 
 const createTestTrees = async (model, fileData) => {
   let n1, n2, n3, n4, n5, n6, n7, n8
-   model.emit('drivesCreated', [ {uuid: uuids.uuid1, type: 'private',owner: uuids.userUUID}, 
-                                 {uuid: uuids.uuid9, type: 'private', owner: uuids.aliceUUID},
-                                 {uuid: uuids.uuid10, type: 'public', writelist: [Uuuids.userUUID], readlist: [uuids.bobUUID], shareAllowed: false},
-                                 {uuid: uuids.uuid11, type: 'public', writelist: [uuids.aliceUUID], readlist: [uuids.bobUUID], shareAllowed: true}
-                               ])
-    await Promise.delay(200)
+  model.emit('drivesCreated', [
+    { uuid: uuidMap.uuid1, type: 'private', owner: uuidMap.userUUID },
+    { uuid: uuidMap.uuid9, type: 'private', owner: uuidMap.aliceUUID },
+    { uuid: uuidMap.uuid10, type: 'public', writelist: [uuidMap.userUUID], readlist: [uuidMap.bobUUID], shareAllowed: false },
+    { uuid: uuidMap.uuid11, type: 'public', writelist: [uuidMap.aliceUUID], readlist: [uuidMap.bobUUID], shareAllowed: true }
+  ])
+  await Promise.delay(200)
 
-    // drive node
-    n1 = fileData.root.children[0]
-    // n9 = fileData.root.children[1]
+  // drive node
+  n1 = fileData.root.children[0]
+  // n9 = fileData.root.children[1]
 
-    fileData.createNode(n1, {type: 'directory', uuid: uuids.uuid2, name: 'n2'})
-    await Promise.delay(100)
-    n2 = fileData.uuidMap.get(uuids.uuid2)
-    // console.log(n2.parent.name)
-    fileData.createNode(n2, {type: 'directory', uuid: uuids.uuid3, name: 'n3'})
-    await Promise.delay(100)
-    n3 = fileData.uuidMap.get(uuids.uuid3)
-    // console.log(n3.parent.name)
-    fileData.createNode(n3, {type: 'directory', uuid: uuids.uuid4, name: 'n4'})
-    await Promise.delay(100)
-    n4 = fileData.uuidMap.get(uuids.uuid4)
-    // console.log(n4.parent.name)
-    fileData.createNode(n1, {type: 'directory', uuid: uuids.uuid5, name: 'n5'})
-    await Promise.delay(100)
-    n5 = fileData.uuidMap.get(uuids.uuid5)
-    // console.log(n5.parent.name)
-    fileData.createNode(n1, {type: 'directory', uuid: uuids.uuid6, name: 'n6'})
-    await Promise.delay(100)
-    n6 = fileData.uuidMap.get(uuids.uuid6)
-    // console.log(n6.parent.name)
-    fileData.createNode(n6, {type: 'directory', uuid: uuids.uuid7, name: 'n7'})
-    await Promise.delay(100)
-    n7 = fileData.uuidMap.get(uuids.uuid7)
-    // console.log(n7.parent.name)
-    fileData.createNode(n7, {type: 'directory', uuid: uuids.uuid8, name: 'n8'})
-    await Promise.delay(100)
-    n8 = fileData.uuidMap.get(uuids.uuid8)
+  fileData.createNode(n1, { type: 'directory', uuid: uuidMap.uuid2, name: 'n2' })
+  await Promise.delay(100)
+  n2 = fileData.uuidMap.get(uuidMap.uuid2)
+  // console.log(n2.parent.name)
+  fileData.createNode(n2, { type: 'directory', uuid: uuidMap.uuid3, name: 'n3' })
+  await Promise.delay(100)
+  n3 = fileData.uuidMap.get(uuidMap.uuid3)
+  // console.log(n3.parent.name)
+  fileData.createNode(n3, { type: 'directory', uuid: uuidMap.uuid4, name: 'n4' })
+  await Promise.delay(100)
+  n4 = fileData.uuidMap.get(uuidMap.uuid4)
+  // console.log(n4.parent.name)
+  fileData.createNode(n1, { type: 'directory', uuid: uuidMap.uuid5, name: 'n5' })
+  await Promise.delay(100)
+  n5 = fileData.uuidMap.get(uuidMap.uuid5)
+  // console.log(n5.parent.name)
+  fileData.createNode(n1, { type: 'directory', uuid: uuidMap.uuid6, name: 'n6' })
+  await Promise.delay(100)
+  n6 = fileData.uuidMap.get(uuidMap.uuid6)
+  // console.log(n6.parent.name)
+  fileData.createNode(n6, { type: 'directory', uuid: uuidMap.uuid7, name: 'n7' })
+  await Promise.delay(100)
+  n7 = fileData.uuidMap.get(uuidMap.uuid7)
+  // console.log(n7.parent.name)
+  fileData.createNode(n7, { type: 'directory', uuid: uuidMap.uuid8, name: 'n8' })
+  await Promise.delay(100)
+  n8 = fileData.uuidMap.get(uuidMap.uuid8)
 }
 
 module.exports = {
-  uuids,
+  uuidMap,
   model,
-  createTestTrees,
+  createTestTrees
 }
 
 
