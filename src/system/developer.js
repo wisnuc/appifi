214b99143dd18e0a43321fa4b8bd4371064c983c
matianfu <matianfu@gmail.com>
2017-04-09 15:25:01 +0800

    add fake bootstrap, change system server port back to 3000

--- src/system/developer.js
+++ src/system/developer.js
@@ -1,10 +1,18 @@
+const child = require('child_process')
+
 const setting = {}
 
 process.argv.forEach((val, index, array) => {
 
   if (val === '--no-fruitmix') setting.noFruitmix = true
   if (val === '--appstore-master') setting.appstoreMaster = true
+  if (val === '--fake-bootstrap') {
 
+    console.log('fake bootsrap advertising')
+    setting.fakeBootstrap = true
+    child.exec('avahi-set-host-name wisnuc-generic-deadbeef')    
+    child.spawn('avahi-publish-service', ['fakeBootstrap', '_http._tcp', 3002], { stdio: 'ignore' })
+  }
 })
 
 module.exports = setting
