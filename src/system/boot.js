c84d7aed2a54b513be0c08e453f164373393d14a
matianfu <matianfu@gmail.com>
2017-04-10 16:46:42 +0800

    tmp

--- src/system/boot.js
+++ src/system/boot.js
@@ -101,6 +101,7 @@ const cfs = fsys => ({ type: fsys.fileSystemType, uuid: fsys.fileSystemUUID, mou
 module.exports = {
 
   data: null,
+  fruitmix: null,
 
   probedStorageAsync: async function () {
     let storage = Storage.get()
@@ -109,8 +110,10 @@ module.exports = {
     return storage
   },
 
-  bootAsync: async function (cfs, init) {
-   	await fruitmix.forkAsync(cfs, init) 
+  boot(cfs) {
+
+   	this.fruitmix = fruitmix.fork(cfs) 
+    this.data = { state: 'normal', currentFileSystem: cfs }
     Config.updateLastFileSystem({type: cfs.type, uuid: cfs.uuid})
   },
 
@@ -133,14 +136,12 @@ module.exports = {
         try {
           assertFileSystemGood(fsys)
           assertReadyToBoot(fsys.wisnuc)
-          await this.bootAsync(cfs(fsys))
-          this.data = { state: 'normal', currentFileSystem: cfs(fsys) }
+          this.boot(cfs(fsys))
         }
         catch (e) {
 					console.log('[autoboot] failed to boot lastfs', last, e)
           this.data = { state: 'maintenance', error: 'EFAIL', message: e.message }
         }
-
     		console.log('[autoboot] boot state', this.data)
         return
       }
@@ -161,34 +162,32 @@ module.exports = {
       } 
     })
 
-    if (alts.length !== 1) {
+    if (alts.length === 1) 
+      this.boot(cfs(alts[0]))
+    else 
       this.data = { state: 'maintenance', error: alts.length === 0 ? 'ENOALT' : 'EMULTIALT' }
-    }
-    else {
-      await this.bootAsync(cfs(alts[0]))
-      this.data = { state: 'alternative', currentFileSystem: cfs(alts[0]) }
-    }
 
     console.log('[autoboot] boot state', this.data)
   },
 
   // manual boot only occurs in maintenance mode.
   // this operation should not update boot state if failed.
+  // target: file system UUID
+  // username, password, if install is true or reinstall is true
   manualBootAsync: async function (args) {
 
     if (this.data.state !== 'maintenance') throw new Error('not in maintenance mode')    
 
-
-    let { type, uuid, username, password, install, reinstall } = args 
+    let { target, username, password, install, reinstall } = args 
 
     let storage = await Storage.refreshAsync() 
     let fileSystems = extractFileSystems(storage)
 
-    let fsys = fileSystems.find(f => f.type === type && f.uuid === uuid)
+    let fsys = fileSystems.find(f => f.uuid === target)
     if (!fsys) throw Object.assign(new Error('target not found'), { code: 'ENOENT' })
 
     assertFileSystemGood(fsys)
-    let wisnuc = await probeAsync(fsys)
+    let wisnuc = await fruitmix.probeAsync(fsys.mountpoint)
 
     if (reinstall === true || install === true) {
       if (reinstall) {
@@ -198,14 +197,12 @@ module.exports = {
       else {
         assertReadyToInstall(wisnuc)
       }
-      await this.bootAsync(cfs(fsys), { username, password })
     }
     else { // direct boot, fruitmix status must be 'READY'
       assertReadyToBoot(wisnuc) 
-      await this.bootAsync(cfs(fsys))
     }
 
-    this.data = { state: 'normal', currentFileSystem: cfs(fsys) }
+    this.boot(cfs(fsys))
   },
 
   // reboot
