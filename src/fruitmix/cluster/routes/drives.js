acf904598e8e23cf575964323295dc15a5f777b9
panda <wangpanhn@qq.com>
2017-04-10 17:10:38 +0800

    islocaluser & drive info API

---
+++ src/fruitmix/cluster/routes/drives.js
@@ -0,0 +1,16 @@
+
+import { Router } from 'express'
+
+import auth from '../middleware/auth'
+
+let router = Router()
+
+router.get('/:driveUUID', auth.jwt(), (req, res) => {
+  let driveUUID = req.params.driveUUID
+  config.ipc.call('getDriveInfo', driveUUID, (err, drive) => {
+    if (err) return res.status(500).json({})
+    res.status(200).json(Object.assign({}, drive))
+  })
+})
+
+export default router
