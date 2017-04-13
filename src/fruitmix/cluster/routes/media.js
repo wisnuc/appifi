2ba1c1770655989bfd55e516f9734af906420fce
jianjin.wu <mosaic101@foxmail.com>
2017-04-13 17:15:17 +0800

    temp

--- src/fruitmix/cluster/routes/media.js
+++ src/fruitmix/cluster/routes/media.js
@@ -1,29 +1,13 @@
-const path = require('path')
-const fs = require('fs')
-const crypto = require('crypto')
-const Transform = require('stream').Transform
 
 const router = require('express').Router()
-const formidable = require('formidable')
-const UUID = require('node-uuid')
-const validator = require('validator')
-const sanitize = require('sanitize-filename')
+import config from '../config'
 
-import paths from '../../lib/paths'
-import config from '../../config'
-
-// list, tree and nav a directory
-router.get('/:type/:dirUUID/:rootUUID', (req, res) => {
+// all metadata
+router.get('/', (req, res) => {
 
   let userUUID = req.user.userUUID
-  let { type, dirUUID, rootUUID } = req.params
 
-  let typeArr = ['list', 'tree' ,'list-nav', 'tree-nav']
-  if (typeArr.indexOf(type) === -1) return res.error(null, 400)
-
-  let args = { userUUID, dirUUID, rootUUID }
-
-  config.ipc.call(type, args, (err, data) => {
+  config.ipc.call('getMeta', userUUID, (err, data) => {
     if (err) return res.error(err)
     return res.success(data) 
   })
