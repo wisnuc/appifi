c731fb6a64b2a12240f96b66e16ac1b10a09df55
jianjin.wu <mosaic101@foxmail.com>
2017-04-19 11:23:05 +0800

    temp

--- src/fruitmix/cluster/routes/drives.js
+++ src/fruitmix/cluster/routes/drives.js
@@ -6,11 +6,20 @@ import config from '../config'
 
 let router = Router()
 
+// get home, library & public drive
+router.get('/', auth.jwt(), (req, res) => {
+	config.ipc.call('getDrives', (err, drives) => {
+		err ? res.status(500).json({})
+			: res.status(200).json(Object.assign({}, drives))
+	})
+})
+
+// get drive info
 router.get('/:driveUUID', auth.jwt(), (req, res) => {
   let driveUUID = req.params.driveUUID
   config.ipc.call('getDriveInfo', driveUUID, (err, drive) => {
-    if (err) return res.status(500).json({})
-    res.status(200).json(Object.assign({}, drive))
+  	err ? res.status(500).json({})
+			: res.status(200).json(Object.assign({}, drive))
   })
 })
 
