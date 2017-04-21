007b32d5350ca8e7001bc38caaab103274c7866d
panda <wangpanhn@qq.com>
2017-04-21 11:13:56 +0800

    fix account api

--- src/fruitmix/cluster/routes/account.js
+++ src/fruitmix/cluster/routes/account.js
@@ -2,12 +2,13 @@
 import { Router } from 'express'
 import config from '../config'
 
+
 let router = Router();
 
 // get account info
 router.get('/', (req, res) => {
 
-	let userUUID = req.user.useruuid;
+	let userUUID = req.user.uuid;
 	config.ipc.call('getAccountInfo', userUUID, (err, user) => {
 		err ? res.status(500).json({})
 			: res.status(200).json(Object.assign({}, user))
