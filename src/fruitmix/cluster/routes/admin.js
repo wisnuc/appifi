a39472eda72fe1c542f655c71611acb86a59e471
matianfu <matianfu@gmail.com>
2017-04-29 20:37:34 +0800

    fix create drive bug

--- src/fruitmix/cluster/routes/admin.js
+++ src/fruitmix/cluster/routes/admin.js
@@ -80,7 +80,7 @@ router.get('/drives', (req, res) => {
 router.post('/drives', (req, res) => {
 	// permission useruuid
 	let useruuid = req.user.uuid;
-	let drive = req.drive;
+	let drive = req.body;
 	config.ipc.call('createPublicDrive', { useruuid, props:drive }, (err, drive) => {
 		err ? res.status(500).json(Object.assign({}, err))
 			: res.status(200).json(Object.assign({}, { drive }))
