d7ff280e7007d6287fb39e987bfe6023affa5f83
jianjin.wu <mosaic101@foxmail.com>
2017-05-09 11:09:43 +0800

    temp

--- src/fruitmix/cluster/routes/media.js
+++ src/fruitmix/cluster/routes/media.js
@@ -1,7 +1,11 @@
-
+const path = require('path')
+const fs = require('fs')
 const UUID = require('node-uuid')
+const formidable = require('formidable')
 const router = require('express').Router()
+
 import config from '../config'
+import { DIR } from '../../../lib/const'
 
 // get mata data of all I can view
 router.get('/', (req, res) => {
@@ -62,7 +66,65 @@ router.get('/:digest/thumbnail', (req, res) => {
       return res.status(200).sendFile(ret)
     }
   })
+})
 
+
+
+// old libraries api
+router.post('/:digest', (req, res) => {
+  let userUUID = req.user.uuid
+  let libraryUUID = req.user.library
+
+  let sha256 = req.params.digest
+
+  let args = { sha256, libraryUUID, src: '', check: true }
+
+  config.ipc.call('createLibraryFile', args, err => {
+    if(err){
+      if(err.code === 'EEXIST')
+        return res.success(null, 200)
+      
+      return res.error(err, 500)
+    }
+    let form = new formidable.IncomingForm()
+    form.hash = 'sha256'
+    let abort = false
+    form.on('fileBegin', (name, file) => {
+      file.path = path.join(config.path, DIR.TMP, UUID.v4()) 
+    })
+
+    form.on('file', (name, file) => {
+      if (abort) return
+      if (sha256 !== file.hash) {
+        return fs.unlink(file.path, err => {
+          res.status(500).json({
+            code: 'EAGAIN',
+            message: 'sha256 mismatch'
+          })
+        })
+      }
+
+      args = { sha256, libraryUUID, src: file.path, check: false }
+      config.ipc.call('createLibraryFile', args, (err, data) => {
+        if(err) return res.error(err)
+        let entry = {
+          digest: sha256,
+          ctime: new Date().getTime()
+        }
+        return res.success(entry, 200)
+      })
+
+    })
+
+    form.on('error', err => {
+      if (abort) return
+      abort = true
+      return res.status(500).json({})  // TODO
+    })
+
+    form.parse(req)
+
+  })
 })
 
 module.exports = router
