983ccc180aea6053cc53652ec5296e12d9ba7704
CJackYang <ijackyang@sina.cn>
2017-07-13 11:21:18 +0800

    tmp

--- src/fruitmix/station/lib/tickets.js
+++ src/fruitmix/station/lib/tickets.js
@@ -2,19 +2,20 @@ const request = require('superagent')
 
 const { FILE, CONFIG } = require('./const')
 
-let createTicket = (data, sa, callback) => {
+let createTicket = (data, sa, type, callback) => {
   //TODO encrypt data
   request
     .post(CONFIG.CLOUD_PATH + 'v1/tickets')
     .set('Content-Type', 'application/json')
     .send({
        stationId: sa.id,
-       data: '123456'
+       data: '123456',
+       creator: data.uuid,
+       type
     })
     .end((err, res) => {
-      if(err || res.status !== 200) return callback(new Error('register error')) 
-      console.log(res.body)
-      return callback(null, res.body)
+      if(err || res.status !== 200) return callback(new Error('register error'))
+      return callback(null, res.body.data)
     }) 
 }
 
--- src/fruitmix/station/route/tickets.js
+++ src/fruitmix/station/route/tickets.js
@@ -8,9 +8,8 @@ let router = Router()
 //create
 router.post('/', (req, res) => {
   let user = req.user
-  tickets.createTicket(user, req.body.sa, (err, resp) => {
-    console.log(err)
-    console.log(resp)
+  let type = req.body.type
+  tickets.createTicket(user, req.body.sa, type, (err, resp) => {
     if(err) return res.status(500).json(err)
     return res.status(200).json(resp)
   })
