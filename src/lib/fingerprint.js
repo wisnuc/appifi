4f4ae4b16726e3a0618884123784e372f28dbb7b
matianfu <matianfu@gmail.com>
2017-07-28 12:47:02 +0800

    threadify extracted, createBigFile battery included

--- src/lib/fingerprint.js
+++ src/lib/fingerprint.js
@@ -2,7 +2,10 @@ const Promise = require('bluebird')
 const path = require('path')
 const fs = Promise.promisifyAll(require('fs'))
 const child = require('child_process')
+const EventEmitter = require('events')
 const crypto = require('crypto')
+
+const debug = require('debug')('fingerprint')
 
 // constant
 const SIZE_1G = 1024 * 1024 * 1024
@@ -37,51 +40,8 @@ process.on('message', message => {
 
 fs.writeFileSync(childModulePath, childSource)
 
-const Mixin = base => class extends base {
-
-  constructor(...args) {
-    super(...args)
-    this._untils = []
-  }
-
-  async untilAsync (predicate, ignore) {
-    if (predicate()) return
-    return new Promise((resolve, reject) => this._untils.push({ 
-      predicate, 
-      resolve, 
-      reject: ignore ? null : reject
-    }))
-  }
-
-  _until () {
-    this._untils = this._untils.reduce((arr, x) => (this.error && x.reject) 
-      ? K(arr)(x.reject())
-      : x.predicate() 
-        ? K(arr)(x.resolve()) 
-        : [...arr, x], [])
-  }
-
-  observe (name, value) {
-    let _name = '_' + name
-    this[_name] = value
-    Object.defineProperty(this, name, {
-      get: function () {
-        return this[_name]
-      },
-      set: function (x) { 
-        if (this[_name]) return // TODO
-        console.log('observe set', name, x)
-        this[_name] = x
-        process.nextTick(() => this._until())
-      }
-    })
-  }
-}
-
-// class HashWorker extends Mixin(
 
 const chop = number => {
-
   let arr = []
   for (; number > SIZE_1G; number -= SIZE_1G) {
     let start = arr.length === 0 ? 0 : arr[arr.length - 1].end
@@ -95,46 +55,62 @@ const chop = number => {
 
 const combineHash = bufs => bufs.length === 1 
   ? bufs[0] 
-  : combineHash([
-      crypto.createHash('sha256').update(bufs[0]).update(bufs[1]).digest(), 
-      ...bufs.slice(2)
-    ])
+  : combineHash([crypto.createHash('sha256').update(bufs[0]).update(bufs[1]).digest(), ...bufs.slice(2)])
+
 
 const fingerprintAsync = async filePath => {
 
-  let stat1 = await fs.lstatAsync(filePath)
-  if (!stat1.isFile()) throw new Error('not a file')
-  if (stat1.size === 0) return ZERO_HASH
+  let workers = []
 
-  let segments = chop(stat1.size) 
-  let buffers = await Promise.map(segments, async segment => new Promise((resolve, reject) => {
+  try {
+    let stat1 = await fs.lstatAsync(filePath)
+    if (!stat1.isFile()) throw new Error('not a file')
+    if (stat1.size === 0) return ZERO_HASH
 
-    let error, digest, worker = child.fork(childModulePath)
-    worker.on('message', message => digest = message)
-    worker.on('error', err => {
-      if (error) return
-      error = err
-      worker.kill()
-      reject(err)
-    })
-    worker.on('exit', (code, signal) => {
-      if (code || signal || !digest) return reject(new Error('error exit code or killed'))
-      resolve(Buffer.from(digest, 'hex')) 
-    })
-    worker.send({ filePath, start: segment.start, end: segment.end })
-  }), { concurrency: 4 })
+    let segments = chop(stat1.size) 
 
-  let stat2 = await fs.lstatAsync(filePath)
-  if (stat2.mtime.getTime() !== stat1.mtime.getTime()) throw new Error('timestamp mismatch')
+    // bluebird promise.map has concurrency option
+    let buffers = await Promise.map(segments, async segment => new Promise((resolve, reject) => {
 
-  return combineHash(buffers).toString('hex')
+      let error
+      const setError = err => {
+        if (error) return
+        error = err
+        reject(err)
+      }
+
+      let digest
+      let worker = child.fork(childModulePath)
+      worker.on('message', message => digest = message)
+      worker.on('error', err => setError(err))
+      worker.on('exit', (code, signal) => {
+        if (error) return
+        if (code || signal || !digest) {
+          setError(new Error('error exit code or being killed'))
+        } else {
+          resolve(Buffer.from(digest, 'hex')) 
+        }
+      })
+      worker.send({ filePath, start: segment.start, end: segment.end })
+      workers.push(worker)
+
+    }), { concurrency: 4 })
+
+    let stat2 = await fs.lstatAsync(filePath)
+    if (stat2.mtime.getTime() !== stat1.mtime.getTime()) throw new Error('timestamp mismatch')
+
+    return combineHash(buffers).toString('hex')
+
+  } finally {
+    workers.forEach(worker => worker.kill())
+  }
 }
 
 const fingerprint = (filePath, callback) => 
   fingerprintAsync(filePath)
     .then(digest => callback(null, digest))
     .catch(e => callback(e))
-    
+
 module.exports = fingerprint
 
 if (process.argv.includes('--standalone')) {
--- src/media/media.js
+++ src/media/media.js
@@ -1,7 +1,25 @@
 
 /**
-  hash -> metadata
+hash -> metadata
 */
 const map = new Map()
 
+const registerFile = (fingerprint, uuid, filePath) => {
+  if (map.has(fingerprint)) return
+
+  // retrieve xstat ???
+  // retrieve  
+  //
+}
+
+const registerBlob = (fingerprint, filePath) => {
+  if (map.has(fingerprint)) return
+  
+  // retrieve file type, if not interested type return
+  // retrieve metadata and update map
+}
+
+const metadata = fingerprint => map.get(fingerprint) 
+
+module.exports = { registerFile, registerBlob, metadata }
 
