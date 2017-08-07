46276cf065e0471633f6a9a87d1675f714fd8d9a
CJackYang <ijackyang@sina.cn>
2017-08-07 05:23:07 -0400

    tmp

--- src/station/lib/pipe.js
+++ src/station/lib/pipe.js
@@ -1,14 +1,55 @@
 const { upload, download } = require('./request')
-
-const handleUpload = () => {
+const Connect = require('./connect')
+const broadcast = require('../../common/broadcast')
+// const handleUpload = () => {
   
+// }
+
+// const handleDownload = () => {
+
+// }
+
+// module.exports = {
+//   handleUpload,
+//   handleDownload
+// }
+
+class Fetch {
+  constructor() {
+
+  }
+
+  run() {
+
+  }
 }
 
-const handleDownload = () => {
+class Store {
+  constructor() {
 
+  }
+
+  run() {
+
+  }
 }
 
-module.exports = {
-  handleUpload,
-  handleDownload
+
+class Pipe{
+  constructor() {
+    broadcast.on('FruitmixStart', froot => this.froot = froot)
+    broadcast.on('Connect_Connected', () => {
+      Connect.register('pipe', this.handle.bind(this))
+    })
+  }
+
+  handle(data) {
+    if(data.type === 'fetch')
+      return new Fetch(data.data)
+    if(data.type === 'store')
+      return new Store(data.data)
+    throw new Error('PIPE Type not find!')
+  }
 }
+
+module.exports = new Pipe()
