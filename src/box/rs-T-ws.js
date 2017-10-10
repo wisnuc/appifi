8c7d7780b3c0dca40047f67d13942c3731cb2dfd
fangqiaoyun <854079504@qq.com>
2017-10-10 18:13:19 +0800

    tmp

--- src/box/rs-T-ws.js
+++ src/box/rs-T-ws.js
@@ -10,28 +10,23 @@ class HashTransform extends Transform {
   constructor() {
     super()
     this.hashStream = crypto.createHash('sha256')
-    this.length = 0
+    this.size = 0
     this.total = 0
-    this.fingerprint = undefined
+    this.hashArr = []
   }
 
   _transform(buf, enc, next) {
-    if (this.length + buf.length >= SIZE_1G) {
-      let len = SIZE_1G - this.length
+    if (this.size + buf.length > SIZE_1G) {
+      let len = SIZE_1G - this.size
       this.hashStream.update(buf.slice(0, len))
-      let hash = this.hashStream.digest()
+      this.hashArr.push(this.hashStream.digest())
 
-      if (!this.fingerprint)
-        this.fingerprint = hash
-      else 
-        this.fingerprint = crypto.createHash('sha256').update(this.fingerprint).update(hash).digest()
-
-      this.length = buf.slice(len).length
+      this.size = buf.slice(len).length
       this.hashStream = crypto.createHash('sha256')
       this.hashStream.update(buf.slice(len))
     } else {
       this.hashStream.update(buf)
-      this.length += buf.length
+      this.size += buf.length
     }
 
     this.total += buf.length
@@ -39,105 +34,40 @@ class HashTransform extends Transform {
     next()
   }
   
-  // _flush(next) {
-  //   this.digest = this.hashStream.digest('hex')
-  //   next()
-  // }
+  _flush(next) {
+    this.hashArr.push(this.hashStream.digest())
+    next()
+  }
 }
 
 
-// method_1
+// method_1     rs -> T -> ws
 class Transfer {
-  constructor() {
-    this.hashArr = []
-  }
+  constructor() {}
 
   storeFile(src, dst, callback) {
     let rs = fs.createReadStream(src)
-    let ws = fs.createWriteStream(dst, { flags: 'a'})
+    let ws = fs.createWriteStream(dst)
     let hashMaker = new HashTransform()
-    rs.pipe(hashMaker)
-    let finished = false
+    rs.pipe(hashMaker).pipe(ws)
 
-    let error = (err) => {
-      if (finished) return
-      finished = true
-      return callback(err)
-    }
-
-    let finish = (hashArr) => {
-      if (finished) return
-      finished = true
-      return callback(null, hashArr)
-    }
-
-    let abort = () => {
-      if (finished) return
-      finished = true
-      return callback(new Error('ABORT'))
-    }
-
-    console.log(hashMaker.length)
-    rs.on('data', data => {
-      let chunk = Buffer.from(data)
-
-      if (hashMaker.length + chunk.length >= SIZE_1G) {
-        rs.pause()
-        // 1. write data to full
-        // 2. update hash and push into hashArr
-        // 3. write the rest of data
-        let len = SIZE_1G - hashMaker.length
-        console.log('len',len)
-        console.log(1111, hashMaker.length)
-        // write data to full
-        hashMaker.write(chunk.slice(0, len))
-        console.log('lenght1',hashMaker.length)
-        // update hash in hashArr
-        let digest = hashMaker.getHash()
-        this.hashArr.push(digest)
-        console.log(this.hashArr)
-        // write the rest of data
-        hashMaker = new HashTransform()
-
-        console.log('new hashMaker is ready')
-        console.log('length2',hashMaker.length)
-        hashMaker.pipe(ws)
-        hashMaker.write(chunk.slice(len))
-        console.log('length3',hashMaker.length)
-        
-        console.log('aaaa')
-        rs.resume()
-
-        // push hash into hashArr
-        // this.hashArr.push(hashMaker.getHash())
-      } else {
-        hashMaker.write(data)
-      }
-    })
-
-    rs.on('close', () => {
-      ws.close()
-      console.log('closed')
-      let digest = hashMaker.getHash()
-      this.hashArr.push(digest)
-      console.log(this.hashArr)
-      if (this.hashArr.length === 1) finish(digest)
+    hashMaker.on('end', () => {
+      console.log('end')
+      if (hashMaker.hashArr.length === 1) return callback(null, hashMaker.hashArr[0].toString('hex'))
       else {
-        hashMaker = new HashTransform()
-        hashMaker.write(this.hashArr[0])
-        for(let i = 1; i < this.hashArr.length; i++) {
-          hashMaker.write(this.hashArr[i])
-          digest = hashMaker.getHash()
-          if (i === this.hashArr.length - 1) finish(digest)
+        let hash = crypto.createHash('sha256')
+        hash.update(hashMaker.hashArr[0])
+        for(let i = 1; i < hashMaker.hashArr.length; i++) {
+          hash.update(hashMaker.hashArr[i])
+          let digest = hash.digest()
+          if (i === hashMaker.hashArr.length - 1) return callback(null, digest.toString('hex'))
           else {
-            hashMaker = new HashTransform()
-            hashMaker.write(digest)
+            hash = crypto.createHash('sha256')
+            hash.update(digest)
           }
         }
-      }     
+      }
     })
-
-    rs.on('error', err => error(err))
   }
 }
 
@@ -160,10 +90,10 @@ class Transfer_1 {
       return callback(err)
     }
 
-    let finish = (hashArr) => {
+    let finish = (fingerprint) => {
       if (finished) return
       finished = true
-      return callback(null, hashArr)
+      return callback(null, fingerprint)
     }
 
     let abort = () => {
@@ -175,7 +105,7 @@ class Transfer_1 {
     rs.on('data', data => {
       let chunk = Buffer.from(data)
 
-      if (size + chunk.length >= SIZE_1G) {
+      if (size + chunk.length > SIZE_1G) {
         rs.pause()
 
         // write data to full, update hash
@@ -195,11 +125,10 @@ class Transfer_1 {
       }
     })
 
-    rs.on('close', () => {
-      console.log('closed')
+    rs.on('end', () => {
+      console.log('end')
       let digest = hashMaker.digest()
       this.hashArr.push(digest)
-      console.log(this.hashArr)
       if (this.hashArr.length === 1) finish(digest.toString('hex'))
       else {
         hashMaker = crypto.createHash('sha256')
@@ -221,24 +150,65 @@ class Transfer_1 {
 }
 
 // method_3: rs -> ws     (write to destination)
-//              -> T      (calculate hash)
+//           rs -> T      (calculate hash)
 class Transfer_2 {
-  constructor() {
-    this.hashArr = []
-  }
+  constructor() {}
 
-  storeFile(src, dst) {
+  storeFile(src, dst, callback) {
+    let rs = fs.createReadStream(src)
+    let ws = fs.createWriteStream(dst)
+    let hashMaker = new HashTransform()
+
+    rs.pipe(ws)
+    rs.pipe(hashMaker)
+
+    hashMaker.on('end', () => {
+      console.log('end')
+      console.log('ws bytes：',ws.bytesWritten)
+      console.log('hashMaker bytes: ', hashMaker.total)
+      if (ws.bytesWritten !== hashMaker.total)
+        return callback(new Error('size mismatch'))
+      else {
+        if (hashMaker.hashArr.length === 1) return callback(null, hashMaker.hashArr[0].toString('hex'))
+        else {
+          let hash = crypto.createHash('sha256')
+          hash.update(hashMaker.hashArr[0])
+          for(let i = 1; i < hashMaker.hashArr.length; i++) {
+            hash.update(hashMaker.hashArr[i])
+            let digest = hash.digest()
+            if (i === hashMaker.hashArr.length - 1) return callback(null, digest.toString('hex'))
+            else {
+              hash = crypto.createHash('sha256')
+              hash.update(digest)
+            }
+          }
+        }
+      }
+    })
+
 
   }
 }
 
 
-let fpath = '/home/laraine/Projects/appifi/testdata/vpai001.jpg'
-let dst = '/home/laraine/Projects/appifi/tmptest/vpai001.jpg'
+// let fpath = '/home/laraine/Projects/appifi/testdata/vpai001.jpg'
+// let dst = '/home/laraine/Projects/appifi/tmptest/vpai001.jpg'
 
-// let fpath = '/home/laraine/Projects/appifi/testdata/hello'
-// let dst = '/home/laraine/Projects/appifi/tmptest/hello'
+// let fpath = '/home/laraine/Projects/appifi/test-files/two-giga'
+// let dst = '/home/laraine/Projects/appifi/tmptest/two-giga'
 
-let worker = new Transfer_1()
-worker.storeFile(fpath, dst, (err, hashArr) => console.log('hashArr', hashArr))
+// let fpath = '/home/laraine/Projects/appifi/test-files/two-and-a-half-giga'
+// let dst = '/home/laraine/Projects/appifi/tmptest/two-and-a-half-giga'
+
+// let fpath = '/home/laraine/Projects/appifi/test-files/two-giga-minus-1'
+// let dst = '/home/laraine/Projects/appifi/tmptest/two-giga-minus-1'
+
+let fpath = '/home/laraine/Projects/appifi/test-files/two-giga-plus-x'
+let dst = '/home/laraine/Projects/appifi/tmptest/two-giga-plus-x'
+
+let worker = new Transfer_2()
+worker.storeFile(fpath, dst, (err, fingerprint) => {
+  if (err) console.log(err)
+  console.log('fingerprint', fingerprint)
+})
 
