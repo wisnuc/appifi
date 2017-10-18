fab6dbb2fc7aa6d2045f0877663d4c4c1c7bfc49
lewis ma <lewis.ma@winsuntech.cn>
2017-10-18 14:53:38 +0800

    lint readdir

--- src/forest/readdir.js
+++ src/forest/readdir.js
@@ -1,11 +1,9 @@
+const debug = require('debug')('fruitmix:readdir')
+
 const path = require('path')
 const fs = require('fs')
-
-const debug = require('debug')('readdir')
-
 const { readXstat } = require('../lib/xstat')
 const Monitor = require('./monitor')
-
 
 /**
 `readdir` reads the contents of directories and updates `Directory` children and mtime accordingly.
@@ -26,8 +24,7 @@ class Idle {
   /**
   @param {Directory} dir - directory object
   */
-  constructor(dir) {
-
+  constructor (dir) {
     /**
     hosting directory object
     */
@@ -37,22 +34,24 @@ class Idle {
   }
 
   /** Implement `read` virtual method, see `Directory` for definition. */
-  read(handler) {
-    if (Number.isInteger(handler))
+  read (handler) {
+    if (Number.isInteger(handler)) {
       this.dir.readdir = new Pending(this.dir, handler)
-    else if (typeof handler === 'function' || handler instanceof Monitor)
+    } else if (typeof handler === 'function' || handler instanceof Monitor) {
       this.dir.readdir = new Working(this.dir, [handler])
-    else if (handler === undefined)
+    } else if (handler === undefined) {
       this.dir.readdir = new Working(this.dir, [])
-    else
+    } else {
       throw new Error('invalid handler')
+    }
   }
 
   /**
   Abort. Do nothing.
   */
-  abort() {
+  abort () {
   }
+
 }
 
 /**
@@ -64,10 +63,8 @@ class Pending {
   @param {Directory} dir - directory object
   @param {number} delay - time to delay, in ms.
   */
-  constructor(dir, delay) {
-
-    if (!Number.isInteger(delay) || delay < 0) 
-      throw new Error('invalid delay argument')
+  constructor (dir, delay) {
+    if (!Number.isInteger(delay) || delay < 0) { throw new Error('invalid delay argument') }
 
     /**
     hosting directory object
@@ -81,22 +78,26 @@ class Pending {
   }
 
   /** Implement `read` virtual method, see `Directory` for definition. */
-  read(handler) {
+  read (handler) {
     clearTimeout(this.timer)
-    if (Number.isInteger(handler))
-      this.timer = setTimeout(() => this.dir.readdir = new Working(this.dir), handler)
-    else if (typeof handler === 'function' || handler instanceof Monitor)
+    if (Number.isInteger(handler)) {
+      this.timer = setTimeout(() => {
+        this.dir.readdir = new Working(this.dir)
+      }, handler)
+    } else if (typeof handler === 'function' || handler instanceof Monitor) {
       this.dir.readdir = new Working(this.dir, [handler])
-    else if (handler === undefined) 
+    } else if (handler === undefined) {
       this.dir.readdir = new Working(this.dir, [])
-    else
+    } else {
       throw new Error('invalid handler')
+    }
   }
 
   /** Abort. Clear timer. **/
-  abort() {
+  abort () {
     clearTimeout()
   }
+
 }
 
 /**
@@ -113,8 +114,7 @@ class Working {
   @param {Directory} dir - directory object
   @param {Array} [handlers] - an array of handlers to be serviced by this instance
   */
-  constructor(dir, handlers) {
-    
+  constructor (dir, handlers) {
     /**
     hosting directory object
     */
@@ -123,13 +123,13 @@ class Working {
     /**
     comparing timestamp or not
     */
-    this.compareTimestamp = handlers ? false : true
+    this.compareTimestamp = !handlers
 
     /**
     handlers to be serviced by this operation
     @type {Array}
     */
-    this.handlers = handlers ? handlers : []
+    this.handlers = handlers || []
 
     // tell monitors a readdir starts
     this.handlers
@@ -142,7 +142,7 @@ class Working {
     @type {(number|Array)}
     */
     this.pending = undefined
-    
+
     /**
     Keep a copy of directory's abspath before `readdir` for detecting race. 
     */
@@ -157,7 +157,7 @@ class Working {
   Check if the given error is a path error
   @param {error} err
   */
-  isPathError(err) {
+  isPathError (err) {
     return ['ENOENT', 'ENOTDIR', 'EINSTANCE', 'EINTERRUPTED'].includes(err.code)
   }
 
@@ -165,17 +165,16 @@ class Working {
   Decorate `statusCode: 503` to path error
   @param {error} err
   */
-  decorate(err) {
+  decorate (err) {
     if (this.isPathError(err)) err.statusCode = 503
   }
 
   /**
   Request immediate `read` on all ancestors along node path (exclusive).
   */
-  fixPath() {
-
+  fixPath () {
     // ancestors (exclusive)
-    let ancestors = []    
+    let ancestors = []
     for (let n = this.dir.parent; n !== null; n = n.parent) ancestors.unshift(n)
     ancestors.forEach(n => n.read())
   }
@@ -188,8 +187,7 @@ class Working {
   @param {number} mtime 
   @param {boolean} transient
   */
-  finish(err, xstats, mtime, transient) {
-
+  finish (err, xstats, mtime, transient) {
     if (this.finished) return
 
     this.finished = true
@@ -198,82 +196,67 @@ class Working {
     if (this.dirPath !== this.dir.abspath()) {
       err = new Error('path changed during readdir operation')
       err.code = 'EINTERRUPTED'
-    } 
+    }
 
     debug(`${this.dir.name} working finished`)
 
     // processing error or result
     if (err) {
-
       this.decorate(err)
 
       if (this.isPathError(err)) {
-
-        this.fixPath() 
+        this.fixPath()
         this.read()
-
-      }
-      else {
-
+      } else {
         // schedule a read TODO backoff
         this.read(150)
       }
-    }
-    else if (xstats) {
+    } else if (xstats) {
+      if (mtime !== this.dir.mtime) { this.dir.merge(xstats, this.handlers.filter(h => h instanceof Monitor)) }
 
-      if (mtime !== this.dir.mtime) 
-        this.dir.merge(xstats, this.handlers.filter(h => h instanceof Monitor))
-
-      if (mtime !== this.dir.mtime && transient === false) 
-        this.dir.mtime = mtime
+      if (mtime !== this.dir.mtime && transient === false) { this.dir.mtime = mtime }
 
       if (transient) this.read(150)
     }
 
     // process handlers
     this.handlers.forEach(handler => {
-      if (typeof handler === 'function') 
-        handler(err, xstats)
-      if (handler instanceof Monitor) 
-        handler.end(this.dir.uuid, this.dir.name)
+      if (typeof handler === 'function') { handler(err, xstats) }
+      if (handler instanceof Monitor) { handler.end(this.dir.uuid, this.dir.name) }
     })
 
     debug(`${this.dir.name} working next`, this.pending, transient)
 
-    if (Array.isArray(this.pending))
+    if (Array.isArray(this.pending)) {
       this.dir.readdir = new Working(this.dir, this.pending)
-    else if (typeof this.pending === 'number' || (xstats && transient))
+    } else if (typeof this.pending === 'number' || (xstats && transient)) {
       this.dir.readdir = new Pending(this.dir, this.pending, Math.min(this.pending, 150))
-    else
+    } else {
       this.dir.readdir = new Idle(this.dir)
+    }
   }
 
   /**
   Read xstats of all files in the given directory. Only regular files and directories are read. 
   @param {function} callback - `(err, xstats)`
   */
-  readXstats(callback) {
-
+  readXstats (callback) {
     fs.readdir(this.dirPath, (err, entries) => {
-
       if (this.finished) return
       if (err) return callback(err)
       if (entries.length === 0) return callback(null, [])
 
-      let xstats = [], count = entries.length
-
+      let xstats = []
+      let count = entries.length
       entries.forEach(ent => {
-
         let entPath = path.join(this.dirPath, ent)
         readXstat(entPath, (err, xstat) => {
-
           if (this.finished) return
           if (!err) xstats.push(xstat)
           if (!--count) {
-            xstats.sort((a,b) => a.name.localeCompare(b.name))          
+            xstats.sort((a, b) => a.name.localeCompare(b.name))
             callback(null, xstats)
           }
-
         })
       })
     })
@@ -282,36 +265,43 @@ class Working {
   /**
   start the `read` operation
   */
-  start() {
-
+  start () {
     readXstat(this.dirPath, (err, xstat1) => {
-
       if (this.finished) return
       if (err) return this.finish(err)
-      if (xstat1.type !== 'directory') 
-        return this.finish(Object.assign(new Error('not a dir'), { code: 'ENOTDIR'}))
-      if (xstat1.uuid !== this.dir.uuid) 
-        return this.finish(Object.assign(new Error('uuid mismatch'), { code: 'EINSTANCE'}))
+      if (xstat1.type !== 'directory') {
+        this.finish(Object.assign(new Error('not a dir'), { code: 'ENOTDIR' }))
+        return
+      }
 
-      if (this.compareTimestamp && xstat1.mtime === this.dir.mtime)
+      if (xstat1.uuid !== this.dir.uuid) {
+        this.finish(Object.assign(new Error('uuid mismatch'), { code: 'EINSTANCE' }))
+        return
+      }
+
+      if (this.compareTimestamp && xstat1.mtime === this.dir.mtime) {
         this.finish(null)
+      }
 
       this.readXstats((err, xstats) => {
-
         if (this.finished) return
         if (err) return this.finish(err)
         readXstat(this.dirPath, (err, xstat2) => {
-
           if (this.finished) return
-          if (err) return this.finish(err)        
-          if (xstat2.type !== 'directory') 
-            return this.finish(Object.assign(new Error('not a dir'), { code: 'ENOTDIR'}))
-          if (xstat2.uuid !== this.dir.uuid) 
-            return this.finish(Object.assign(new Error('uuid mismatch'), { code: 'EINSTANCE'}))
+          if (err) return this.finish(err)
+          if (xstat2.type !== 'directory') {
+            this.finish(Object.assign(new Error('not a dir'), { code: 'ENOTDIR' }))
+            return
+          }
+
+          if (xstat2.uuid !== this.dir.uuid) {
+            this.finish(Object.assign(new Error('uuid mismatch'), { code: 'EINSTANCE' }))
+            return
+          }
 
           this.finish(null, xstats, xstat2.mtime, xstat2.mtime !== xstat1.mtime)
         })
-      }) 
+      })
     })
   }
 
@@ -319,43 +309,40 @@ class Working {
   implement `read` virtual method 
   @param {undefined|number|function|Monitor} handler
   */
-  read(handler) {
-
-    if (handler instanceof Monitor) 
-      handler.start(this.dir.uuid, this.dir.name)
+  read (handler) {
+    if (handler instanceof Monitor) { handler.start(this.dir.uuid, this.dir.name) }
 
     if (Number.isInteger(handler)) {
-      if (this.pending === undefined) 
+      if (this.pending === undefined) {
         this.pending = handler
-      else if (typeof this.pending === 'number') 
+      } else if (typeof this.pending === 'number') {
         this.pending = Math.min(this.pending, handler)
-      else if (Array.isArray(this.pending))
-        return
-      else
+      } else if (Array.isArray(this.pending)) {
+      } else {
         throw new Error('invalid pending type')
-    }
-    else if (typeof handler === 'function' || handler instanceof Monitor) {
-      if (this.pending === undefined)
+      }
+    } else if (typeof handler === 'function' || handler instanceof Monitor) {
+      if (this.pending === undefined) {
         this.pending = [handler]
-      else if (typeof this.pending === 'number') 
+      } else if (typeof this.pending === 'number') {
         this.pending = [handler]
-      else if (Array.isArray(this.pending))
+      } else if (Array.isArray(this.pending)) {
         this.pending.push(handler)
-      else
+      } else {
         throw new Error('invalid pending type')
-    }
-    else if (handler === undefined) {
-      if (this.pending === undefined)
+      }
+    } else if (handler === undefined) {
+      if (this.pending === undefined) {
         this.pending = []
-      else if (typeof this.pending === 'number')
+      } else if (typeof this.pending === 'number') {
         this.pending = []
-      else if (Array.isArray(this.pending))
-        return
-      else
+      } else if (Array.isArray(this.pending)) {
+      } else {
         throw new Error('invalid pending type')
-    }
-    else
+      }
+    } else {
       throw new Error('invalid handler')
+    }
 
     // console.log('readdir read, working', handler, this.pending)
   }
@@ -363,8 +350,7 @@ class Working {
   /** 
   implement `abort` virtual method
   */
-  abort() {
-
+  abort () {
     if (this.finished) return
 
     this.finished = true
@@ -373,12 +359,15 @@ class Working {
     err.statusCode = 503
 
     this.handlers.forEach(handler => {
-      if (typeof handler === 'function') 
+      if (typeof handler === 'function') {
         handler(err)
-      if (handler instanceof Monitor) 
+      }
+      if (handler instanceof Monitor) {
         handler.end(this.dir.uuid, this.dir.name, err)
+      }
     })
   }
+
 }
 
 /**
@@ -386,7 +375,6 @@ Construct a `readdir` state machine, starting from `Working` state.
 
 @param {Directory} dir - Directory object
 */
-const readdir = (dir, monitors) => new Working(dir, monitors || []) 
+const readdir = (dir, monitors) => new Working(dir, monitors || [])
 
 module.exports = readdir
-
