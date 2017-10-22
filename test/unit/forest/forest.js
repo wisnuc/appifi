bbbcfbfa842a944f24a2e7bd98f007fb21c9b81b
lewis ma <lewis.ma@winsuntech.cn>
2017-10-22 23:26:15 +0800

    more tests

--- test/unit/forest/forest.js
+++ test/unit/forest/forest.js
@@ -12,6 +12,10 @@ const should = chai.should()
 
 const DriveList = require('src/forest/forest')
 const Monitor = require('src/forest/monitor')
+const Directory = require('src/forest/directory')
+const File = require('src/forest/file')
+
+const { FILES } = require('test/agent/lib')
 
 const { readXstatAsync, forceDriveXstatAsync } = require('src/lib/xstat')
 
@@ -84,6 +88,103 @@ describe(path.basename(__filename), () => {
         driveList.on('indexingDone', () => {
           console.log(driveList)
           done()
+        })
+      })
+    })
+
+  })
+
+  describe('hello/word/foo/bar', () => {
+
+    let mediaMap
+    let driveList
+    let { vpai001 } = FILES
+ 
+    beforeEach(async () => {
+      await rimrafAsync(tmptest)
+      await mkdirpAsync(tmpDir)
+      await mkdirpAsync(drivesDir)
+      await mkdirpAsync(path.join(drivesDir, drive1.uuid, 'hello', 'world', 'foo', 'bar'))
+
+      let monitor = new Monitor()
+      mediaMap = new Map()
+      driveList = new DriveList(tmptest, mediaMap)
+      await driveList.createDriveAsync(drive1, [monitor])
+      await monitor.done
+
+    })
+
+    it('put vpai001.jpg into root 4720f130', done => {
+      let src = path.join(cwd, 'testdata', 'vpai001.jpg')
+      let dst = path.join(drivesDir, drive1.uuid, 'vpai001.jpg')
+
+      fs.copyFile(src, dst, err => {
+        if (err) return done(err)
+        
+        let [ _, root] = driveList.roots[Symbol.iterator]().next().value
+        driveList.on('indexingDone', () => {
+          expect(root.children.length).to.equal(2)
+
+          // assert vpai File object
+          let vpai = root.children.find(c => c instanceof File)
+          expect(vpai.name).to.equal('vpai001.jpg')
+          expect(vpai.magic).to.equal('JPEG')
+          expect(vpai.hash).to.equal(vpai001.hash) 
+          expect(vpai.finger).to.be.null
+          expect(vpai.fingerFail).to.equal(0)
+          expect(vpai.meta).to.be.null
+          expect(vpai.metaFail).to.equal(0)
+
+          // assert meta map
+          expect(driveList.metaMap.size).to.equal(1)          
+          expect(driveList.metaMap.get(vpai.hash).size).to.equal(1)
+          expect(driveList.metaMap.get(vpai.hash).has(vpai)).to.be.true
+
+          done() 
+        })
+
+        root.read()
+      })
+    })
+
+    it('put vpai001.jpg twice into root b7adebab', done => {
+      let src = path.join(cwd, 'testdata', 'vpai001.jpg')
+      let dst1 = path.join(drivesDir, drive1.uuid, 'vpai001.jpg')
+      let dst2 = path.join(drivesDir, drive1.uuid, 'vpai002.jpg')
+
+      fs.copyFile(src, dst1, err => {
+        if (err) return done(err)
+        fs.copyFile(src, dst2, err => {
+          let [ _, root] = driveList.roots[Symbol.iterator]().next().value
+          driveList.on('indexingDone', () => {
+            expect(root.children.length).to.equal(3)
+
+            // assert vpai File object
+            let vpai1 = root.children.find(c => c instanceof File && c.name === 'vpai001.jpg')
+            expect(vpai1.magic).to.equal('JPEG')
+            expect(vpai1.hash).to.equal(vpai001.hash) 
+            expect(vpai1.finger).to.be.null
+            expect(vpai1.fingerFail).to.equal(0)
+            expect(vpai1.meta).to.be.null
+            expect(vpai1.metaFail).to.equal(0)
+
+            let vpai2 = root.children.find(c => c instanceof File && c.name === 'vpai001.jpg')
+            expect(vpai2.magic).to.equal('JPEG')
+            expect(vpai2.hash).to.equal(vpai001.hash) 
+            expect(vpai2.finger).to.be.null
+            expect(vpai2.fingerFail).to.equal(0)
+            expect(vpai2.meta).to.be.null
+            expect(vpai2.metaFail).to.equal(0)
+
+            // assert meta map
+            expect(driveList.metaMap.size).to.equal(1)          
+            expect(driveList.metaMap.get(vpai001.hash).size).to.equal(2)
+            expect(driveList.metaMap.get(vpai001.hash).has(vpai1)).to.be.true
+            expect(driveList.metaMap.get(vpai001.hash).has(vpai2)).to.be.true
+            done() 
+          })
+
+          root.read()
         })
       })
     })
