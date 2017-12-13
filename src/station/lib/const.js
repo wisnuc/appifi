c0dcb4d679cd34d300ca3ab9305e8aeeae7c80d6
jianjin.wu <mosaic101@foxmail.com>
2017-12-13 10:50:25 +0800

    modify clientId

--- src/station/lib/const.js
+++ src/station/lib/const.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'dev') {
   CONFIG.MQTT_URL = 'mqtt://10.10.9.78:1883'
 } else if (process.env.NODE_ENV === 'test') {
   CONFIG.CLOUD_PATH = 'http://10.10.9.78:4000/'
-  CONFIG.MQTT_URL = 'mqtt://10.10.9.78:1883'
+  CONFIG.MQTT_URL = 'mqtt://122.152.206.50:1883'
 } else {
   CONFIG.CLOUD_PATH = 'http://www.siyouqun.org/'
   CONFIG.MQTT_URL = 'mqtt://122.152.206.50:1883'
