79aa163b677903f3cf71fa53dec96b9b450b7b8c
CJackYang <ijackyang@sina.cn>
2017-12-20 11:09:15 +0800

    tmp

--- src/station/lib/pipe.js
+++ src/station/lib/pipe.js
@@ -271,8 +271,8 @@ class Pipe {
         break
     }
   }
-
-
+  /************************** TICKET *************************/
+  
   /*****************************TOKEN***************************/
 
   async getTokenAsync(data) {
@@ -441,6 +441,21 @@ class Pipe {
     if(!fruit) return await this.errorResponseAsync(serverAddr, sessionId, new Error('fruitmix not start'))
     let asyncMkdir = Promise.promisify(fruit.mkdirp).bind(fruit)
     let xstat = await asyncMkdir(user, body.driveUUID, body.dirUUID, body.toName)
+    debug('mkdirp success', xstat)
+    return await this.successResponseJsonAsync(serverAddr, sessionId, xstat)
+  }
+
+  async mkdirpAsync2 (data) {
+    let { serverAddr, sessionId, user, body, paths } = data
+    let fruit = getFruit()
+    if (!fruit) return await this.errorResponseAsync(serverAddr, sessionId, new Error('fruitmix not start'))
+    let dst = {
+      drive: body.driveUUID,
+      dir: body.dirUUID,
+      name: body.toName
+    }
+    let asyncMkdir = Promise.promisify(fruit.driveList.mkdir).bind(fruit.driveList)
+    let xstat = await asyncMkdir(dst, null)
     debug('mkdirp success', xstat)
     return await this.successResponseJsonAsync(serverAddr, sessionId, xstat)
   }
