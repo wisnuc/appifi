fbd81dec51d75e90d3ed83d6c9dc74e69ec9c3ea
lewis ma <lewis.ma@winsuntech.cn>
2017-12-28 18:36:08 +0800

    add btrfs test

--- test/unit/lib/btrfs2.js
+++ test/unit/lib/btrfs2.js
@@ -1,6 +1,7 @@
 const Promise = require('bluebird')
 const path = require('path')
 const fs = require('fs')
+const child = require('child_process')
 const crypto = require('crypto')
 
 const rimrafAsync = Promise.promisify(require('rimraf'))
@@ -26,7 +27,7 @@ describe(path.basename(__filename), () => {
   beforeEach(async () => {
     await rimrafAsync(tmptest)
     await mkdirpAsync(tmptest)
-  })
+  })  
 
   it('clone one giga', function (done) {
     this.timeout(0)
@@ -36,8 +37,8 @@ describe(path.basename(__filename), () => {
     btrfsConcat(target, [FILES.oneGiga.path], err => {
       console.timeEnd('btrfsConcat')
       done(err)
-    })
-  })
+    })  
+  })  
 
   it('clone one twice (output incorrect)', function (done) {
     this.timeout(0)
@@ -52,18 +53,141 @@ describe(path.basename(__filename), () => {
       btrfsConcat(target, [FILES.oneGiga.path], err => {
         console.timeEnd('btrfsConcat')
         done(err) 
-      })
-    })
-  })
+      })  
+    })  
+  })  
 
-  it('clone one twice (output incorrect)', function (done) {
+  it('clone one twice (output)', function (done) {
     this.timeout(0)
     let target = path.join(tmptest, UUID.v4())
 
     console.time('btrfsConcat')
     btrfsConcat(target, [FILES.oneGiga.path, FILES.oneGiga.path], err => {
       console.timeEnd('btrfsConcat')
-      done(err) 
+      done(err)
+    })
+  })  
+
+  it('clone one twice (newly written)', function (done) {
+    this.timeout(0)
+    let target = path.join(tmptest, UUID.v4())
+
+    const buf = Buffer.allocUnsafe(1024 * 1024 * 1024)
+
+    let tmpfile = path.join(tmptest, UUID.v4())
+    let ws = fs.createWriteStream(tmpfile)
+    ws.write(buf)
+    ws.end(() => {
+      console.time('btrfsConcat')
+      btrfsConcat(target, [FILES.oneGiga.path, tmpfile], err => {
+        console.timeEnd('btrfsConcat')
+        done(err)
+      })
     })
   })
+
+  it('clone one twice (newly written), sync before btrfsConcat', function (done) {
+    this.timeout(0)
+    let target = path.join(tmptest, UUID.v4())
+
+    const buf = Buffer.allocUnsafe(1024 * 1024 * 1024)
+
+    let tmpfile = path.join(tmptest, UUID.v4())
+    let ws = fs.createWriteStream(tmpfile)
+    ws.write(buf)
+    ws.end(() => {
+      child.exec('sync', err => {
+        console.time('btrfsConcat')
+        btrfsConcat(target, [FILES.oneGiga.path, tmpfile], err => {
+          console.timeEnd('btrfsConcat')
+          done(err)
+        })
+      })
+    })
+  })
+
+  it('clone one twice (newly written), fsync/reopen before btrfsConcat', function (done) {
+    this.timeout(0)
+    let target = path.join(tmptest, UUID.v4())
+
+    const buf = Buffer.allocUnsafe(1024 * 1024 * 1024)
+
+    let tmpfile = path.join(tmptest, UUID.v4())
+    let ws = fs.createWriteStream(tmpfile)
+    ws.write(buf)
+    ws.end(() => {
+
+      fs.open(tmpfile, 'r', (err, fd) => {
+        if (err) return done(err)
+        fs.fsync(fd, err => {
+          if (err) return done(err)
+          fs.close(fd, () => {})
+
+          console.time('btrfsConcat')
+          btrfsConcat(target, [FILES.oneGiga.path, tmpfile], err => {
+            console.timeEnd('btrfsConcat')
+            done(err)
+          })
+
+        })
+      })
+    })
+  })
+
+  it('clone one twice (newly written), fsync/autoclose false before btrfsConcat', function (done) {
+    this.timeout(0)
+    let target = path.join(tmptest, UUID.v4())
+
+    const buf = Buffer.allocUnsafe(1024 * 1024 * 1024)
+
+    let tmpfile = path.join(tmptest, UUID.v4())
+    let ws = fs.createWriteStream(tmpfile, { autoClose: false })
+
+    ws.write(buf)
+    ws.end(() => {
+      fs.fsync(ws.fd, err => {
+        if (err) return done(err)
+        fs.close(ws.fd, () => {})
+
+        console.time('btrfsConcat')
+        btrfsConcat(target, [FILES.oneGiga.path, tmpfile], err => {
+          console.timeEnd('btrfsConcat')
+          done(err)
+        })
+
+      })
+    })
+  })
+
+  it('clone one twice (copied file)', function (done) {
+    this.timeout(0)
+    let target = path.join(tmptest, UUID.v4())
+    let tmpfile = path.join(tmptest, UUID.v4())
+    fs.copyFile(FILES.oneGiga.path, tmpfile, err => {
+      if (err) return done(err)
+      console.time('btrfsConcat')
+      btrfsConcat(target, [FILES.oneGiga.path, tmpfile], err => {
+        console.timeEnd('btrfsConcat')
+        done(err)
+      })
+    })
+  })
+
+  it('clone one twice (copied file), sync before btrfsConcat', function (done) {
+    this.timeout(0)
+    let target = path.join(tmptest, UUID.v4())
+    let tmpfile = path.join(tmptest, UUID.v4())
+    fs.copyFile(FILES.oneGiga.path, tmpfile, err => {
+      if (err) return done(err)
+      child.exec('sync', err => {
+        console.time('btrfsConcat')
+        btrfsConcat(target, [FILES.oneGiga.path, tmpfile], err => {
+          console.timeEnd('btrfsConcat')
+          done(err)
+        })
+      })
+    })
+  })
+
 })
+
