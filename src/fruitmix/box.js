fb212d49cf3f6448c829fc47290336b58992d21e
CJackYang <ijackyang@sina.cn>
2018-01-25 17:44:27 +0800

    tmp

--- src/fruitmix/box.js
+++ src/fruitmix/box.js
@@ -28,7 +28,7 @@ module.exports = {
   },
 
   reportMedia(fingerprint, metadata) {
-    this.mediaMap.setMetadata(fingerprint, metadata)
+    this.mediaMap.set(fingerprint, metadata)
   },
 
   userCanReadBlob(user, fingerprint) {
@@ -279,7 +279,7 @@ module.exports = {
     let tweets = await box.getTweetsAsync(props)
     if (metadata) {
       tweets.forEach(t => 
-        t.type === 'list' ? (t.list.forEach(l => this.mediaMap.hasMetadata(l.sha256) ? l.metadata = this.mediaMap.getMetadata(l.sha256) : l)) :t
+        t.type === 'list' ? (t.list.forEach(l => this.mediaMap.has(l.sha256) ? l.metadata = this.mediaMap.get(l.sha256) : l)) :t
       )
     }
     return tweets
