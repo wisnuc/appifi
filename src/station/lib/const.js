a33e3f6e9c026bac56f21e2145795cda18460528
CJackYang <ijackyang@sina.cn>
2018-01-26 11:07:10 +0800

    change mqtt path

--- src/station/lib/const.js
+++ src/station/lib/const.js
@@ -13,10 +13,10 @@ if (process.env.NODE_ENV === 'dev') {
   CONFIG.MQTT_URL = 'mqtt://10.10.9.79:1883'
 } else if (process.env.NODE_ENV === 'test') {
   CONFIG.CLOUD_PATH = 'http://122.152.206.50/'
-  CONFIG.MQTT_URL = 'mqtt://122.152.206.50:1883'
+  CONFIG.MQTT_URL = 'mqtt://test.siyouqun.com:1883'
 } else {
   CONFIG.CLOUD_PATH = 'http://www.siyouqun.com/'
-  CONFIG.MQTT_URL = 'mqtt://122.152.206.50:1883'
+  CONFIG.MQTT_URL = 'mqtt://mqtt.siyouqun.com:1883'
 }
 
 Object.freeze(FILE)
