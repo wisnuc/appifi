24aac66b300da52839c6e66d3cc3fa81670a18ba
CJackYang <ijackyang@sina.cn>
2018-03-30 16:59:01 +0800

    tmp

--- src/station/lib/const.js
+++ src/station/lib/const.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'dev') {
   CONFIG.CLOUD_PATH = 'http://10.10.9.87:4000/'
   CONFIG.MQTT_URL = 'mqtt://test.siyouqun.com:1883'
 } else if (process.env.NODE_ENV === 'test') {
-  CONFIG.CLOUD_PATH = 'http://test.siyouqun.com/'
+  CONFIG.CLOUD_PATH = 'http://test.siyouqun.com:4000/'
   CONFIG.MQTT_URL = 'mqtt://test.siyouqun.com:1883'
 } else {
   CONFIG.CLOUD_PATH = 'http://www.siyouqun.com/'
