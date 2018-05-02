d51698f63d9a0c0390f8cdee48e475815a4f46bb
lewis ma <lewis.ma@winsuntech.cn>
2018-05-02 12:05:05 +0800

    frameworks of media, tmp

---
+++ src/fruitmix/apis/media.js
@@ -0,0 +1,12 @@
+
+class MediaApi {
+
+  constructor () {
+  }
+
+  GET (user, props, callback) {
+    process.nextTick(() => callback(new Error('not implemented yet'))
+  }    
+}
+
+module.exports = MediaApi
