6aa764f916a66162260f42297ce21a47a3b8fcc2
lewis ma <lewis.ma@winsuntech.cn>
2018-05-06 11:16:34 +0800

    tmp

--- src/fruitmix/NFS.js
+++ src/fruitmix/NFS.js
@@ -7,12 +7,15 @@ NFS provides native file system access to users.
 @module NFS
 */
 
+
+
 class NFS extends EventEmitter {
 
   /**
   Create a NFS module
 
   @param {object} opts
+  @param {object} boundVolume
   @param {object} opts.allowATA 
   @param {object} opts.allowUSB
   */
@@ -20,14 +23,35 @@ class NFS extends EventEmitter {
     super()
     this.allowATA = false
     this.allowUSB = true
+
+    /**
+    a drive is acturally a file system that:
+    1. can be a volume, a standalone disk, or a partition
+    */
     this.drives = []
   }
 
   update (storage) {
-    
+ 
   }
 
-  
+  LIST (user, props, callback) {
+  }
+
+  GET (user, props, callback) {
+  }    
+
+  POSTFORM (user, props, callback) {
+  } 
+
+  PATCH (user, props, callback) {
+  }
+
+  PUT (user, props, callback) {
+  }
+
+  DELETE (user, props, callback) {
+  }
 }
 
 module.exports = NFS
