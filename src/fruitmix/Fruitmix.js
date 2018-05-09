a357e6b4e10574d8a36763f9ab375ae93d60e3e5
CJackYang <ijackyang@sina.cn>
2018-05-09 17:21:35 +0800

    tmp

--- src/fruitmix/Fruitmix.js
+++ src/fruitmix/Fruitmix.js
@@ -175,7 +175,7 @@ class Fruitmix extends EventEmitter {
     this.user.on('Update', () => {
       process.send(JSON.stringify({
         type: 'appifi_users',
-        users: this.users.map(u => { return { uid: u.phicommUserId }})
+        users: this.users.filter(x => !x.isFirstUser).map(u => { return { uid: u.phicommUserId }})
       }))
       this.emit('FruitmixStarted')
     })
